678881eab54db353fa4aa80a02634e2f3666e41f
Dean Silfen <dean.silfen@gmail.com>
2014-06-02 11:11:36 -0400

    removed sets because os.walk returns a tuple with a string and two lists which cased an issue

--- sift_encode.py
+++ sift_encode.py
@@ -6,11 +6,11 @@ root = "."
 
 def watch(root):
     '''watching the current directory'''
-    before = set(os.walk('.')) 
+    before = [paths for paths in os.walk('.')] 
     while True:
-        after = set(os.walk('.')) 
-        added = after - before
-        removed = before - after
+        after = [paths for paths in os.walk('.')] 
+        added = [paths for paths in after if paths not in before]   
+        removed = [paths for paths in before if paths not in after]
         for paths in added:
             if 'streamers' in paths[0]:
                 print 'streaming directory, ignoring...' 
@@ -22,22 +22,22 @@ def watch(root):
 def crawl_folder(paths):
     '''crawl filesystem and apply encoding function'''
     root, dirs, files = paths
-    for filename in files:
+    for filename in files: #search the folder for a file with .mts extension
         if '.MTS' in filename:
             try: 
-                os.mkdir(root + '/streamers') # consider os.join
+                os.mkdir(root + '/streamers') #create streaming folder in filesystem
             except OSError:
                 pass
             finally:
                 os.chdir(root)
-                map(encode, files)
-                break
+                map(encode, files) #map encode over each dict of files
+                break   
 
 def encode(contents):
-    if contents.lower().endswith('.mts'):
-        new_name = contents[:-4]+'_streamer.mp4'
-        command = "ffmpeg -v verbose -i "+ contents +" -acodec copy -vf 'field, scale=iw/2:ih, setsar=1' -vcodec libx264 -g 60 -crf 30 -threads 8 -preset slow -y streamers/" + new_name 
-        call(command, shell=True) #calll ffmpeg and place new file in new dict
+        if contents.lower().endswith('.mts'): #check to see if MPEG transit stream
+            new_name = contents[:-4]+'_streamer.mp4'
+            command = "ffmpeg -v verbose -i "+ contents +" -acodec copy -vf 'field, scale=iw/2:ih, setsar=1' -vcodec libx264 -g 60 -crf 30 -threads 8 -preset slow -y streamers/" + new_name 
+            call(command, shell=True) #call ffmpeg and place new file in new dict
         #Beware files only play in VLC
 
 if __name__=='__main__':
