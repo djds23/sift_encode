c867f251b33b189c9d747ae2d55e1e8134cceed9
Local Admin <ladmin@radio02.local>
2014-06-02 16:43:53 -0400

    working now, but it re runs for existing files whenever there is a new file dropped. this must be fixed

--- sift_encode.py
+++ sift_encode.py
@@ -22,22 +22,20 @@ def watch(root):
 def crawl_folder(paths):
     '''crawl filesystem and apply encoding function'''
     root, dirs, files = paths
-    for filename in files: #search the folder for a file with .mts extension
-        if '.MTS' in filename:
-            try:
-                new_files= os.path.join(root, 'streamers')
-                os.mkdir(new_files) 
-            except OSError:
-                pass
-            finally:
-                map(encode, files, root) 
-                break   
+    if any('.mts' in contents.lower() for contents in files):
+        try:
+            new_files= os.path.join(root, 'streamers')
+            os.mkdir(new_files) 
+        except OSError:
+            pass
+        finally:
+            filepaths = [os.path.join(root, contents) for contents in files]
+            map(encode, filepaths) 
 
-def encode(contents, root):
+def encode(contents):
         if contents.lower().endswith('.mts'): #check to see if MPEG transit stream
-            new_contents = os.path.join(root, contents)
             new_name = contents[:-4]+'_streamer.mp4'
-            command = "ffmpeg -v verbose -i "+ new_contents +" -acodec copy -vf 'field, scale=iw/2:ih, setsar=1' -vcodec libx264 -g 60 -crf 30 -threads 8 -preset slow -y streamers/" + new_name 
+            command = "ffmpeg -v verbose -i "+ contents +" -acodec copy -vf 'field, scale=iw/2:ih, setsar=1' -vcodec libx264 -g 60 -crf 30 -threads 8 -preset slow -y streamers/" + new_name 
             call(command, shell=True) #call ffmpeg and place new file in new dict
         #Beware files only play in VLC
 
